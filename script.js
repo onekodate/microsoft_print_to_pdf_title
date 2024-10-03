46db4aa69e876d1fa306b43e2e929c284d1aeea0
onekodate <onekodate@gmail.com>
2024-10-03 22:24:50 +0900

    table

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const elem=(id)=>document.getElementById(id);
 
+
+
 const kanji_priority=(char)=>{
     const kanji = [
         '一右雨円王音下火花貝学気九休玉金空月犬見五口校左三山子四糸字耳七車手十出女小上森人水正生青夕石赤千川先早草足村大男竹中虫町天田土二日入年白八百文木本名目立力林六',
@@ -43,28 +45,327 @@ const expand_and_sort=(char)=>Array.from(
     (a, b)=>kanji_priority(a)-kanji_priority(b)
 );
 
-const make_candidates=(string)=>{
-    const candidates = Array.from(string).map(char=>expand_and_sort(char));
-    const tr = (idx) => {
+const range=(length)=>Array.from(
+    {length:length},
+    (v,k)=>k,
+);
+
+const shift_jis=(()=>{
+    const decoder = new TextDecoder("shift-jis")
+    return range(128).map(
+        code=>String.fromCharCode(code)
+    ).join("") + range(256).filter(
+        v=> (
+            0x81<= v && v <=0x84
+        ) || (
+            0x87<= v && v <=0x9f
+        ) || (
+            0xe0<= v && v <=0xea
+        ) || (
+            0xed<= v && v <=0xee
+        ) || (
+            0xfa<= v && v <=0xfc
+        )
+    ).map(
+        i=>range(256).filter(
+            v=> 0x40 <= v && v <= 0xfc
+        ).map(
+            j=>decoder.decode(new Uint8Array([i, j]))
+        ).filter(
+            char=>char.length===1 && char !== '\ufffd'
+        ).join("")
+    ).join("")
+})();
+
+const characters = (()=>range(256).map(
+    i=>range(256).map(
+        j=>String.fromCharCode(
+            j*0x0100+i
+        )
+    ).filter(
+        char=>shift_jis.indexOf(char)>-1
+    ).sort(
+        (a,b)=>kanji_priority(a)-kanji_priority(b)
+    ).join("")
+))();
+
+const restore_dict = (()=>{
+    const restore_dict = {
+        12288:0,
+        1:1,
+        2:2,
+        3:3,
+        4:4,
+        5:5,
+        6:6,
+        7:7,
+        8:8,
+        9:9,
+        // 32:10,
+        11:11,
+        12:12,
+        // 32:13,
+        14:14,
+        15:15,
+        16:16,
+        17:17,
+        18:18,
+        19:19,
+        20:20,
+        21:21,
+        22:22,
+        23:23,
+        24:24,
+        25:25,
+        26:26,
+        27:27,
+        28:28,
+        29:29,
+        30:30,
+        31:31,
+        32:32,
+        33:33,
+        34:34,
+        35:35,
+        36:36,
+        37:37,
+        38:38,
+        39:39,
+        40:40,
+        41:41,
+        42:42,
+        43:43,
+        44:44,
+        45:45,
+        46:46,
+        47:47,
+        48:48,
+        49:49,
+        50:50,
+        51:51,
+        52:52,
+        53:53,
+        54:54,
+        55:55,
+        56:56,
+        57:57,
+        58:58,
+        59:59,
+        60:60,
+        61:61,
+        62:62,
+        63:63,
+        64:64,
+        65:65,
+        66:66,
+        67:67,
+        68:68,
+        69:69,
+        70:70,
+        71:71,
+        72:72,
+        73:73,
+        74:74,
+        75:75,
+        76:76,
+        77:77,
+        78:78,
+        79:79,
+        80:80,
+        81:81,
+        82:82,
+        83:83,
+        84:84,
+        85:85,
+        86:86,
+        87:87,
+        88:88,
+        89:89,
+        90:90,
+        91:91,
+        92:92,
+        93:93,
+        94:94,
+        95:95,
+        96:96,
+        97:97,
+        98:98,
+        99:99,
+        100:100,
+        101:101,
+        102:102,
+        103:103,
+        104:104,
+        105:105,
+        106:106,
+        107:107,
+        108:108,
+        109:109,
+        110:110,
+        111:111,
+        112:112,
+        113:113,
+        114:114,
+        115:115,
+        116:116,
+        117:117,
+        118:118,
+        119:119,
+        120:120,
+        121:121,
+        122:122,
+        123:123,
+        124:124,
+        125:125,
+        126:126,
+        127:127,
+        8226:128,
+        8224:129,
+        8225:130,
+        8230:131,
+        8212:132,
+        8211:133,
+        402:134,
+        8260:135,
+        8249:136,
+        8250:137,
+        8722:138,
+        8240:139,
+        8222:140,
+        8220:141,
+        8221:142,
+        8216:143,
+        8217:144,
+        8218:145,
+        8482:146,
+        64257:147,
+        64258:148,
+        321:149,
+        338:150,
+        352:151,
+        376:152,
+        381:153,
+        305:154,
+        322:155,
+        339:156,
+        353:157,
+        382:158,
+        65533:159,
+        8364:160,
+        161:161,
+        162:162,
+        163:163,
+        164:164,
+        165:165,
+        166:166,
+        167:167,
+        168:168,
+        169:169,
+        170:170,
+        171:171,
+        172:172,
+        173:173,
+        174:174,
+        175:175,
+        176:176,
+        177:177,
+        178:178,
+        179:179,
+        180:180,
+        181:181,
+        182:182,
+        183:183,
+        184:184,
+        185:185,
+        186:186,
+        187:187,
+        188:188,
+        189:189,
+        190:190,
+        191:191,
+        192:192,
+        193:193,
+        194:194,
+        195:195,
+        196:196,
+        197:197,
+    };
+    Object.keys(restore_dict).forEach(key=>{
+        restore_dict[key]=characters[restore_dict[key]];
+    });
+    restore_dict[32]=Array.from(
+        [
+            characters[10],
+            characters[13],
+            characters[32],
+        ].join("")
+    ).sort(
+        (a,b)=>kanji_priority(a)-kanji_priority(b)
+    ).join("");
+    return restore_dict;
+})();
+
+const table_0xc6=()=>{
+    let element = Array.from(characters.slice(0xc6,).join("")).sort(
+        (a,b)=>kanji_priority(a)-kanji_priority(b)
+    );
+    let ncol = 20;
+    let nrow = Math.ceil(element.length/ncol);
+    let tbody = document.createElement("tbody");
+    range(nrow).forEach(i=>{
         const tr = document.createElement("tr");
-        if (idx == 0) tr.id = "tr";
-        candidates.forEach((chars, i)=>{
+        range(ncol).forEach(j=>{
+            if (i*ncol+j < element.length - 1) {
+                const td = document.createElement("td");
+                td.innerText = element[i*ncol+j];
+                tr.append(td);
+            }
+        });
+        tbody.append(tr);
+    });
+    elem("0xc6").replaceChildren(tbody);
+};
+
+const table_0x20=()=>{
+    const element = restore_dict[0x20];
+    const ncol = 10;
+    const nrow = Math.ceil(element.length/ncol);
+    const tbody = document.createElement("tbody");
+    range(nrow).forEach(i=>{
+        const tr = document.createElement("tr");
+        range(ncol).forEach(j=>{
+            if (i*ncol+j < element.length - 1) {
+                const td = document.createElement("td");
+                td.innerText = element[i*ncol+j];
+                tr.append(td);
+            }
+        });
+        tbody.append(tr);
+    });
+    elem("0x20").replaceChildren(tbody);
+};
+
+const table_candidates=(string)=>{
+    const candidates = Array.from(string).map(
+        char=>char.charCodeAt() in restore_dict ?
+            restore_dict[char.charCodeAt()] :
+            char
+    );
+    const tbody = document.createElement("tbody");
+    range(44).forEach(i=>{
+        const tr = document.createElement("tr");
+        if (i == 0) tr.id = "tr";
+        candidates.forEach((chars, j)=>{
             const td = document.createElement("td");
-            const id = i*0x0100 + idx;
-            td.innerHTML = `<input type="radio" id="radio_${id}" name="string_${i}" value="${chars[idx]}" onChange="reflesh_output();" ${idx==0?"checked":""}><label for="radio_${id}">${chars[idx]}</label>`
+            const id = `${i}-${j}`;
+            td.innerHTML = i < chars.length ?
+                `<input type="radio" id="radio_${id}" name="string_${j}" value="${chars[i]}" onChange="reflesh_output();" ${i==0?"checked":""}><label for="radio_${id}">${chars[i]}</label>` :
+                "";
             tr.append(td);
         });
-        return tr;
-    };
-    const tbody = document.createElement("tbody");
-    Array.from(
-        {length: 256},
-        (_, k)=>k,
-    ).forEach(k=>{
-        tbody.append(tr(k));
+        tbody.append(tr);
     });
-    elem("table").innerHTML = "";
-    elem("table").append(tbody);
+    elem("candidates").replaceChildren(tbody);
     reflesh_output();
 };
 
@@ -82,4 +383,6 @@ const reflesh_output = () =>{
     ).join("");
 };
 
-make_candidates(elem("input").value);
+table_candidates(elem("input").value);
+table_0x20();
+table_0xc6();
